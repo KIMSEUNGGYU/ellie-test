50f7e83496796fc5e27b723ecdaeae918933e8b9
kimseunggyu <dev.seunggyu@gmail.com>
2022-03-29 17:26:17 +0900

    [7.3 Auth] 미들웨어 더 작성하기 💡 (실패)

--- dwitter-server/middleware/tests/auth.test.js
+++ dwitter-server/middleware/tests/auth.test.js
@@ -1,5 +1,21 @@
 import httpMocks from "node-mocks-http";
+
+import faker from "faker";
+
 import { isAuth } from "../auth.js";
+
+// 단위 테스트는 다른 모듈의 의존하면 안됨
+// 데이터베이스, 파일 읽기, 네트워크 등 느린 것에 대한 의존성을 가지면 안됨
+
+import jwt from "jsonwebtoken";
+import * as userRepository from "../../data/auth.js";
+// userRepository 는 데이터를 읽어 오기 때문에 로직이 거기에 의존하면 안됨
+// jsonwebtoken 도 토큰을 만들 때 에도 시간이 오래 걸림,
+// => 단위 테스트는 외부에 의존하는 것을 테스트 하는 것이 아닌
+// 느린 데이터베이스에 의존하는 것이 아닌, 다 mocking 해서 사용
+
+jest.mock("jsonwebtoken");
+jest.mock("../../data/auth.js");
 
 describe("Auth Middleware", () => {
   it("returns 401 for the request without Authorization header", async () => {
@@ -9,6 +25,65 @@ describe("Auth Middleware", () => {
     });
     const response = httpMocks.createResponse();
     const next = jest.fn();
+
+    await isAuth(request, response, next);
+
+    expect(response.statusCode).toBe(401);
+    expect(response._getJSONData().message).toBe("Authentication Error");
+    expect(next).not.toBeCalled();
+  });
+
+  it("returns 401 for the request with unsupported Authorization header", async () => {
+    const request = httpMocks.createRequest({
+      method: "GET",
+      url: "/tweets",
+      headers: { Authorization: "Basic" },
+    });
+    const response = httpMocks.createResponse();
+    const next = jest.fn();
+
+    await isAuth(request, response, next);
+
+    expect(response.statusCode).toBe(401);
+    expect(response._getJSONData().message).toBe("Authentication Error");
+    expect(next).not.toBeCalled();
+  });
+
+  it("returns 401 for the request with invalid JWT", async () => {
+    const token = faker.random.alphaNumeric(128);
+    const request = httpMocks.createRequest({
+      method: "GET",
+      url: "/tweets",
+      headers: { Authorization: `Bearer ${token}` },
+    });
+    const response = httpMocks.createResponse();
+    const next = jest.fn();
+    jwt.verify = jest.fn((token, secret, callback) => {
+      callback(new Error("bad token"), undefined);
+    });
+
+    await isAuth(request, response, next);
+
+    expect(response.statusCode).toBe(401);
+    expect(response._getJSONData().message).toBe("Authentication Error");
+    expect(next).not.toBeCalled();
+  });
+
+  it("returns 401 when connot find a user by id from the JWT", async () => {
+    const token = faker.random.alphaNumeric(128);
+    const userId = faker.random.alphaNumeric(32);
+    const request = httpMocks.createRequest({
+      method: "GET",
+      url: "/tweets",
+      headers: { Authorization: `Bearer ${token}` },
+    });
+    const response = httpMocks.createResponse();
+    const next = jest.fn();
+    // ✨🔥 jwt 및, userRepository mocking 해서 사용
+    jwt.verify = jest.fn((token, secret, callback) => {
+      callback(undefined, { id: userId });
+    });
+    userRepository.findById = jest.fn((id) => Promise.resolve(undefined));
 
     await isAuth(request, response, next);
 
