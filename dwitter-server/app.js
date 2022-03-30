dce9d058550ea67908e5ab28ad26c0e9878da43e
kimseunggyu <dev.seunggyu@gmail.com>
2022-03-30 18:27:21 +0900

    [9.10] 개별적인 통합 테스트 파일들 만들기 (포트) - port 충돌 문제 해결 - jest 는 수행시 병렬로 처리하기 때문에 각 테스트마다 sequelize.drop 하면 다른 테스트에도 영향을 주는 이슈 아직 미해결 - 우선 주석 처리함

--- dwitter-server/app.js
+++ dwitter-server/app.js
@@ -1,33 +1,33 @@
-import express from 'express';
-import 'express-async-errors';
-import cors from 'cors';
-import morgan from 'morgan';
-import helmet from 'helmet';
-import tweetsRouter from './router/tweets.js';
-import authRouter from './router/auth.js';
-import { config } from './config.js';
-import { initSocket, getSocketIO } from './connection/socket.js';
-import { sequelize } from './db/database.js';
-import { TweetController } from './controller/tweet.js';
-import * as tweetRepository from './data/tweet.js';
+import express from "express";
+import "express-async-errors";
+import cors from "cors";
+import morgan from "morgan";
+import helmet from "helmet";
+import tweetsRouter from "./router/tweets.js";
+import authRouter from "./router/auth.js";
+import { config } from "./config.js";
+import { initSocket, getSocketIO } from "./connection/socket.js";
+import { sequelize } from "./db/database.js";
+import { TweetController } from "./controller/tweet.js";
+import * as tweetRepository from "./data/tweet.js";
 
 const corsOption = {
   origin: config.cors.allowedOrigin,
   optionsSuccessStatus: 200,
 };
 
-export async function startServer() {
+export async function startServer(port) {
   const app = express();
   app.use(express.json());
   app.use(helmet());
   app.use(cors(corsOption));
-  app.use(morgan('tiny'));
+  app.use(morgan("tiny"));
 
   app.use(
-    '/tweets',
+    "/tweets",
     tweetsRouter(new TweetController(tweetRepository, getSocketIO))
   );
-  app.use('/auth', authRouter);
+  app.use("/auth", authRouter);
 
   app.use((req, res, next) => {
     res.sendStatus(404);
@@ -40,8 +40,10 @@ export async function startServer() {
 
   await sequelize.sync();
 
-  console.log('Server is started....');
-  const server = app.listen(config.port);
+  console.log("Server is started....");
+  // startServer 인자로 넘겨온 port 로 설정,
+  // 만약 port 값이 없다면 app.listen 이 가능한 포트를 찾아서 알아서 실행시켜줌 (이런 기능으로 테스트 수행)
+  const server = app.listen(port);
   initSocket(server);
   return server;
 }
---
+++ dwitter-server/tests/integration-tests/auth.test.js
@@ -0,0 +1,149 @@
+// 테스트 전! 서버 시작 및 데이터베이스 초기화! 설정!
+// 테스트 후! 데이터 베이스 깨끗하게 청소해 놓기!
+
+import axios from "axios";
+import faker from "faker";
+
+import { startServer, stopServer } from "../../app.js";
+import { sequelize } from "../../db/database.js";
+import { createNewUserAccount, makeValidUserDetails } from "./auth_utils.js";
+
+describe("Auth APIs", () => {
+  let server;
+  let request;
+  beforeAll(async () => {
+    server = await startServer();
+    request = axios.create({
+      baseURL: `http://localhost:${server.address().port}`,
+      validateStatus: null,
+      // validateStatus null 로 설정 이유는 axios 는 200은 정상, 그 외 300, 400대는 에러를 처리함
+      // 하지만 테스트 할 때는 200 뿐만 아니라 300, 400 모두 의도한 것이기 때문에 error 로 처리하지 않기 위함
+    });
+  });
+
+  afterAll(async () => {
+    // await sequelize.drop(); // sequelize 가 만든 테이블을 다 삭제함
+    await stopServer(server);
+  });
+
+  // afterAll, beforeAll 인 경우, each 로 안하는 이유, each로 하면 정말 독립적인 환경에서 돌아가는 것과 같음
+  // 하지만 모든 테스트 각각 마다 서버시작, db 싱크, db 지우기, 서버 끄기 와 같은 기능을 매 테스트마다 하면 성능적으로 비효율
+  // all 로 auth 와 같은 특정 테스트가 시작할 때 끝날 때 동작하도록 하고, 대신!! 테스트 작성할 때 개별적으로 테스트되므로 명심해서 테스트 코드를 작성해야 함
+
+  describe("POST to /auth/signup", () => {
+    it("returns 201 and authorization token when user details are valid", async () => {
+      const user = makeValidUserDetails();
+
+      const res = await request.post("/auth/signup", user);
+
+      expect(res.status).toBe(201);
+      expect(res.data.token.length).toBeGreaterThan(0);
+    });
+
+    it("returns 409 when username has already been taken", async () => {
+      const user = makeValidUserDetails();
+      const firstSignup = await request.post("/auth/signup", user);
+      expect(firstSignup.status).toBe(201);
+
+      const res = await request.post("/auth/signup", user);
+
+      expect(res.status).toBe(409);
+      expect(res.data.message).toBe(`${user.username} already exists`);
+    });
+
+    // router.post('/signup', validateSignup, authController.signup);
+    // 여기까지 보면 authController.signup 의 기능은 테스트했지만, validateSignup 부분은 아직 테스트 하지 않음
+    // 해당 부분 테스트 validate
+    // jest 에서 변수는 $ 표시 사용
+    test.each([
+      { missingFiledName: "name", expectedMessage: "name is missing" },
+      {
+        missingFiledName: "username",
+        expectedMessage: "username should be at least 5 characters",
+      },
+      { missingFiledName: "email", expectedMessage: "invalid email" },
+      {
+        missingFiledName: "password",
+        expectedMessage: "password should be at least 5 characters",
+      },
+    ])(
+      `returns 400 when $missingFiledName filed is missing`,
+      async ({ missingFiledName, expectedMessage }) => {
+        const user = makeValidUserDetails();
+        delete user[missingFiledName];
+
+        const res = await request.post("/auth/signup", user);
+
+        expect(res.status).toBe(400);
+        expect(res.data.message).toBe(expectedMessage);
+      }
+    );
+
+    it("returns 400 when password is too short", async () => {
+      const user = {
+        ...makeValidUserDetails(),
+        password: "123",
+      };
+
+      const res = await request.post("/auth/signup", user);
+
+      expect(res.status).toBe(400);
+      expect(res.data.message).toBe("password should be at least 5 characters");
+    });
+  });
+
+  describe("POST to /auth/login", () => {
+    it("returns 200 and authorization token when user credentials are valid", async () => {
+      const user = await createNewUserAccount(request);
+
+      const res = await request.post("/auth/login", {
+        username: user.username,
+        password: user.password,
+      });
+
+      expect(res.status).toBe(200);
+      expect(res.data.token.length).toBeGreaterThan(0);
+    });
+
+    it("returns 401 when password is incorrect", async () => {
+      const user = await createNewUserAccount(request);
+      const wrongPassword = user.password.toUpperCase();
+
+      const res = await request.post("/auth/login", {
+        username: user.username,
+        password: wrongPassword,
+      });
+
+      expect(res.status).toBe(401);
+      expect(res.data.message).toMatch("Invalid user or password");
+    });
+
+    it("returns 401 when username is not found", async () => {
+      const someRandomNonExistentUser = faker.random.alpha({ count: 32 });
+
+      const res = await request.post("/auth/login", {
+        username: someRandomNonExistentUser,
+        password: faker.internet.password(10, true),
+      });
+
+      expect(res.status).toBe(401);
+      expect(res.data.message).toMatch("Invalid user or password");
+    });
+  });
+
+  describe("GET /auth/me", () => {
+    it("returns user details when valid token is present in Authorization header", async () => {
+      const user = await createNewUserAccount(request);
+
+      const res = await request.get("/auth/me", {
+        headers: { Authorization: `Bearer ${user.jwt}` },
+      });
+
+      expect(res.status).toBe(200);
+      expect(res.data).toMatchObject({
+        username: user.username,
+        token: user.jwt,
+      });
+    });
+  });
+});
